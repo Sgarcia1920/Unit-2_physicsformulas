bf5d5d7288803ce7cd46bcba940bef6279db86c7
GARCIA ALVAREZ SILVIA ANAHI <I23050337@monclova.tecnm.mx>
2024-02-17 15:35:40 -0600

    Just remove the F for syntax error.

--- Unit 2_physicsformulas/Formula_2.cs
+++ Unit 2_physicsformulas/Formula_2.cs
@@ -4,7 +4,7 @@ namespace Unit_2_physicsformulas
 	internal class Formula_2
 	{
 		//constant field
-		private const double Gravity = 9.80665F;
+		private const double Gravity = 9.80665;
 
 		public double Calculateweight(double mass_2)
 		{
